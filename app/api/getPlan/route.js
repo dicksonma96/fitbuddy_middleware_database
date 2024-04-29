0b2521b3d3bacf5d45ec27599a4b7c1b8fc33fbe
dicksonma <dickson.ma@claritas.asia>
2024-04-29 23:43:13 +0800

    update

--- app/api/getPlan/route.js
+++ app/api/getPlan/route.js
@@ -12,11 +12,44 @@ export async function GET(request) {
         { status: 200 }
       );
 
-    const result =
-      await sql`select * from workout_plan where user_id = ${user_id}`;
+    const result = await sql`SELECT wp.id as workout_plan_id, 
+      unnested.t_id, 
+      tutorial.name, 
+      tutorial.img_url
+FROM workout_plan wp
+LEFT JOIN (
+   SELECT id, unnest(tutorials) as t_id
+   FROM workout_plan 
+   WHERE user_id = ${user_id}
+) AS unnested ON wp.id = unnested.id
+LEFT JOIN LATERAL (
+   SELECT id, name, img_url
+   FROM tutorial 
+   WHERE tutorial.id = unnested.t_id
+) AS tutorial ON true`;
 
+    let groupedData = result.rows;
+    if (result.rows.length) {
+      groupedData = result.rows.reduce((acc, curr) => {
+        const { workout_plan_id, t_id, name, img_url } = curr;
+
+        if (!acc[workout_plan_id]) {
+          acc[workout_plan_id] = {
+            workout_plan_id,
+            tutorials: [],
+          };
+        }
+
+        acc[workout_plan_id].tutorials.push({ t_id, name, img_url });
+
+        return acc;
+      }, {});
+
+      groupedData = Object.values(groupedData);
+    }
+    // console.log(groupedData);
     return NextResponse.json(
-      { data: result.rows, message: "Success" },
+      { data: groupedData, message: "Success" },
       { status: 200 }
     );
   } catch (error) {
